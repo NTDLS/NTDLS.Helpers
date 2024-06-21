59b2d5a5b1b732483a0fbd80eb343443a383eebb
Josh Patterson <josh@ntdls.com>
2024-06-21 16:28:57 -0400

    Added missing solution.

---
+++ NTDLS.Helpers/Formatters.cs
@@ -0,0 +1,29 @@
+﻿namespace NTDLS.Helpers
+{
+    /// <summary>
+    /// Functions for formatting various data types.
+    /// </summary>
+    public class Formatters
+    {
+        /// <summary>
+        /// Formats a file size to b,kb,mb, etc.
+        /// </summary>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static string FileSize(long size)
+        {
+            double s = size;
+
+            string[] format = new string[] { "{0} bytes", "{0} KB", "{0} MB", "{0} GB", "{0} TB", "{0} PB", "{0} EB" };
+
+            int i = 0;
+            while (i < format.Length && s >= 1024)
+            {
+                s = (int)(100 * s / 1024) / 100.0;
+                i++;
+            }
+
+            return string.Format(format[i], s);
+        }
+    }
+}
