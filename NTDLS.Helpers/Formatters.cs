c42eab73abc7f777a3225013590283da988b5ba1
Josh Patterson <josh@ntdls.com>
2024-08-01 16:50:25 -0400

    Added additional file-size formatter overloads.

--- NTDLS.Helpers/Formatters.cs
+++ NTDLS.Helpers/Formatters.cs
@@ -6,24 +6,56 @@
     public class Formatters
     {
         /// <summary>
-        /// Formats a file size to b,kb,mb, etc.
+        /// Formats a file size to B, KB, MB, etc.
         /// </summary>
-        /// <param name="size"></param>
-        /// <returns></returns>
+        public static string FileSize(decimal size)
+            => FileSize((long)size);
+
+        /// <summary>
+        /// Formats a file size to B, KB, MB, etc.
+        /// </summary>
+        public static string FileSize(int size)
+            => FileSize((double)size);
+
+        /// <summary>
+        /// Formats a file size to B, KB, MB, etc.
+        /// </summary>
+        public static string FileSize(float size)
+            => FileSize((double)size);
+
+        /// <summary>
+        /// Formats a file size to B, KB, MB, etc.
+        /// </summary>
         public static string FileSize(long size)
         {
-            double s = size;
-
-            string[] format = new string[] { "{0} bytes", "{0} KB", "{0} MB", "{0} GB", "{0} TB", "{0} PB", "{0} EB" };
+            decimal dSize = size;
+            var format = new string[] { "{0} B", "{0} KB", "{0} MB", "{0} GB", "{0} TB", "{0} PB", "{0} EB" };
 
             int i = 0;
-            while (i < format.Length && s >= 1024)
+            while (i < format.Length && dSize >= 1024)
             {
-                s = (int)(100 * s / 1024) / 100.0;
+                dSize /= 1024;
                 i++;
             }
 
-            return string.Format(format[i], s);
+            return string.Format(format[i], dSize);
+        }
+
+        /// <summary>
+        /// Formats a file size to B, KB, MB, etc.
+        /// </summary>
+        public static string FileSize(double size)
+        {
+            var format = new string[] { "{0} B", "{0} KB", "{0} MB", "{0} GB", "{0} TB", "{0} PB", "{0} EB" };
+
+            int i = 0;
+            while (i < format.Length && size >= 1024)
+            {
+                size = (100 * size / 1024.0) / 100.0;
+                i++;
+            }
+
+            return string.Format(format[i], size);
         }
     }
 }
