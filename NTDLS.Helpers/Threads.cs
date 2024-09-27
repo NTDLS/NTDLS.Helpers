3f2483d0f47e18f32c2785708f0dcee0c63ab7fe
Josh Patterson <Josh@NTDLS.com>
2024-09-27 13:07:02 -0400

    Added thread helpers.

---
+++ NTDLS.Helpers/Threads.cs
@@ -0,0 +1,35 @@
+﻿namespace NTDLS.Helpers
+{
+    /// <summary>
+    /// Various thread helpers.
+    /// </summary>
+    public class Threads
+    {
+        /// <summary>
+        /// Delegate used for StartThread().
+        /// </summary>
+        public delegate void StartThreadDelegate();
+
+        /// <summary>
+        /// Delegate used for StartThread().
+        /// </summary>
+        public delegate void StartThreadDelegate<T>(T proc);
+
+        /// <summary>
+        /// Starts a thread with the given delegate and parameter.
+        /// </summary>
+        /// <typeparam name="T">Type of the parameter to pass to the delegate.</typeparam>
+        /// <param name="param">Parameter to pass to delegate.</param>
+        /// <param name="proc">Delegate function to call the new thread and pass the given parameters.</param>
+        public static void StartThread<T>(T param, StartThreadDelegate<T> proc)
+            => new Thread(() => proc(param)).Start();
+
+        /// <summary>
+        /// Starts a thread with the given delegate and no parameters.
+        /// </summary>
+        /// <typeparam name="T">Type of the parameter to pass to the delegate.</typeparam>
+        /// <param name="proc">Delegate function to call within the new thread.</param>
+        public static void StartThread(StartThreadDelegate proc)
+            => new Thread(() => proc()).Start();
+    }
+}
