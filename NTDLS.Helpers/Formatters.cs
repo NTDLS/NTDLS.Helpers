86c08823e7933d5357a34a4ce77733e27571e782
Josh Patterson <josh@ntdls.com>
2026-09-04 18:02:33 -0400

    Consolidate and refactor FileSize formatting logic
    This reduces code duplication by centralizing the unit calculation and formatting logic into a single method. All `FileSize` overloads now delegate to this core implementation, standardizing internal calculations to `double` and improving maintainability.

--- NTDLS.Helpers/Formatters.cs
+++ NTDLS.Helpers/Formatters.cs
@@ -1,21 +1,23 @@
-﻿namespace NTDLS.Helpers
+namespace NTDLS.Helpers
 {
     /// <summary>
     /// Functions for formatting various data types.
     /// </summary>
     public class Formatters
     {
+        private static readonly string[] _sizeFormats = { "{0} B", "{0} KB", "{0} MB", "{0} GB", "{0} TB", "{0} PB", "{0} EB" };
+
         /// <summary>
         /// Formats a file size to B, KB, MB, etc.
         /// </summary>
         public static string FileSize(decimal size)
-            => FileSize((long)size);
+            => FileSize((double)size);
 
         /// <summary>
         /// Formats a file size to B, KB, MB, etc.
         /// </summary>
         public static string FileSize(decimal size, int decimalPlaces)
-            => FileSize((long)size, decimalPlaces);
+            => FileSize((double)size, decimalPlaces);
 
         /// <summary>
         /// Formats a file size to B, KB, MB, etc.
@@ -45,122 +47,45 @@
         /// Formats a file size to B, KB, MB, etc.
         /// </summary>
         public static string FileSize(ulong size)
-        {
-            int decimalPlaces = 2;
-            decimal dSize = size;
-            var format = new string[] { "{0} B", "{0} KB", "{0} MB", "{0} GB", "{0} TB", "{0} PB", "{0} EB" };
-
-            if (size <= 1024)
-            {
-                decimalPlaces = 0;
-            }
-
-            int i = 0;
-            while (i < format.Length && dSize >= 1024)
-            {
-                dSize /= 1024;
-                i++;
-            }
-
-            return string.Format(format[i], dSize.ToString($"N{decimalPlaces}"));
-        }
+            => FileSize((double)size);
 
         /// <summary>
         /// Formats a file size to B, KB, MB, etc.
         /// </summary>
         public static string FileSize(ulong size, int decimalPlaces)
-        {
-            decimal dSize = size;
-            var format = new string[] { "{0} B", "{0} KB", "{0} MB", "{0} GB", "{0} TB", "{0} PB", "{0} EB" };
-            int i = 0;
-            while (i < format.Length && dSize >= 1024)
-            {
-                dSize /= 1024;
-                i++;
-            }
-
-            return string.Format(format[i], dSize.ToString($"N{decimalPlaces}"));
-        }
+            => FileSize((double)size, decimalPlaces);
 
         /// <summary>
         /// Formats a file size to B, KB, MB, etc.
         /// </summary>
         public static string FileSize(long size)
-        {
-            int decimalPlaces = 2;
-            decimal dSize = size;
-            var format = new string[] { "{0} B", "{0} KB", "{0} MB", "{0} GB", "{0} TB", "{0} PB", "{0} EB" };
-
-            if (size <= 1024)
-            {
-                decimalPlaces = 0;
-            }
-
-            int i = 0;
-            while (i < format.Length && dSize >= 1024)
-            {
-                dSize /= 1024;
-                i++;
-            }
-
-            return string.Format(format[i], dSize.ToString($"N{decimalPlaces}"));
-        }
+            => FileSize((double)size);
 
         /// <summary>
         /// Formats a file size to B, KB, MB, etc.
         /// </summary>
         public static string FileSize(long size, int decimalPlaces)
-        {
-            decimal dSize = size;
-            var format = new string[] { "{0} B", "{0} KB", "{0} MB", "{0} GB", "{0} TB", "{0} PB", "{0} EB" };
-            int i = 0;
-            while (i < format.Length && dSize >= 1024)
-            {
-                dSize /= 1024;
-                i++;
-            }
-
-            return string.Format(format[i], dSize.ToString($"N{decimalPlaces}"));
-        }
+            => FileSize((double)size, decimalPlaces);
 
         /// <summary>
         /// Formats a file size to B, KB, MB, etc.
         /// </summary>
         public static string FileSize(double size)
-        {
-            int decimalPlaces = 2;
-            var format = new string[] { "{0} B", "{0} KB", "{0} MB", "{0} GB", "{0} TB", "{0} PB", "{0} EB" };
-
-            if (size <= 1024)
-            {
-                decimalPlaces = 0;
-            }
-
-            int i = 0;
-            while (i < format.Length && size >= 1024)
-            {
-                size = (100 * size / 1024.0) / 100.0;
-                i++;
-            }
-
-            return string.Format(format[i], size.ToString($"N{decimalPlaces}"));
-        }
+            => FileSize(size, size <= 1024 ? 0 : 2);
 
         /// <summary>
         /// Formats a file size to B, KB, MB, etc.
         /// </summary>
         public static string FileSize(double size, int decimalPlaces)
         {
-            var format = new string[] { "{0} B", "{0} KB", "{0} MB", "{0} GB", "{0} TB", "{0} PB", "{0} EB" };
-
             int i = 0;
-            while (i < format.Length && size >= 1024)
+            while (i < _sizeFormats.Length - 1 && size >= 1024)
             {
-                size = (100 * size / 1024.0) / 100.0;
+                size /= 1024.0;
                 i++;
             }
 
-            return string.Format(format[i], size.ToString($"N{decimalPlaces}"));
+            return string.Format(_sizeFormats[i], size.ToString($"N{decimalPlaces}"));
         }
     }
 }
